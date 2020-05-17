f284e497a20c746a6c267f96519f7cdc4c51cc5c
Mina91 <marina.inokuchi@stud.hn.de>
2020-05-17 12:53:13 +0200

    update

--- testtreiber.cpp
+++ testtreiber.cpp
@@ -1,3 +1,18 @@
 #include "testtreiber.hpp"
 
+// ---------- private Methoden ----------
+void Testtreiber::test1() {
 
+}
+
+/*
+private:
+void test1();
+void test2();
+void test3();
+void test4();
+void test5();
+public:
+explicit Testtreiber();
+~Testtreiber();
+void execute_tests();*/
--- testtreiber.hpp
+++ testtreiber.hpp
@@ -1,7 +1,22 @@
 #ifndef MIN_HEAP_CPP_TESTTREIBER_HPP
 #define MIN_HEAP_CPP_TESTTREIBER_HPP
 
+#include "datenstruktur.hpp"
 
-
+class Testtreiber {
+private:
+// ---------- private Methoden ----------
+    void test1(Heap<T>::Heap heap_variable);
+    void test2();
+    void test3();
+    void test4();
+    void test5();
+    
+public:
+// ---------- public Methoden ----------
+    explicit Testtreiber();
+    ~Testtreiber();
+    void execute_tests();
+};
 
 #endif //MIN_HEAP_CPP_TESTTREIBER_HPP
